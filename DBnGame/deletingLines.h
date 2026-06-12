9796860533128691e1fb92efa33d41a528a79f0b
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-12 10:36:22 +0700

    Change title of deletinglines window

--- DBnGame/deletingLines.h
+++ DBnGame/deletingLines.h
@@ -192,7 +192,7 @@ namespace DBnGame {
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"deletingLines";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"deletingLines";
+			this->Text = L"Удаление Строк";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
