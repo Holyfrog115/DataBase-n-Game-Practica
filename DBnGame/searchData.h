27a3b9bb65c068b9712b1f64e1c71a809a2cc434
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-16 10:39:54 +0700

    Add constructor to class

--- DBnGame/searchData.h
+++ DBnGame/searchData.h
@@ -13,5 +13,21 @@ public:
 	array<int>^ appartmentsNumberRange = gcnew array<int>(2);
 	array<int>^ livingAreaRange = gcnew array<int>(2);
 	array<int>^ totalAreaRange = gcnew array<int>(2);
+	
+	searchData() {
+		addressSearch = "";
+		houseNumberRange[0] = -1;
+		houseNumberRange[1] = -1;
+		commissionYearRange[0] = -1;
+		commissionYearRange[1] = -1;
+		floorsNumberRange[0] = -1;
+		floorsNumberRange[1] = -1;
+		appartmentsNumberRange[0] = -1;
+		appartmentsNumberRange[1] = -1;
+		livingAreaRange[0] = -1;
+		livingAreaRange[1] = -1;
+		totalAreaRange[0] = -1;
+		totalAreaRange[1] = -1;
+	}
 };
 
